026d81b39dd5bedd3333a03c777d2a405ceaabaf
gwenjo <jo.gwen@hotmail.com>
2021-10-22 22:21:49 +0000

    options

--- about/views.py
+++ about/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def index(request):
+    """ A view to return the about us page """
+
+    return render(request, 'about/about.html')
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'product',
+        'user',
+        'rating',
+        'date_posted'
+    )
